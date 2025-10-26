a5f4b82d95b50e9adc6f2b57393a1ea975e6adc8
Vakulenko Heorhii <zhora2vakulenko2@gmail.com>
2025-10-26 18:59:10 +0200

    refactor: moved the map with delivery zones to a new component DeliveryMap

--- client/src/components/pages/DeliveryTerms/DeliveryTerms.tsx
+++ client/src/components/pages/DeliveryTerms/DeliveryTerms.tsx
@@ -1,27 +1,11 @@
-import { blueZones, greenZones, orangeZones, redZones } from '@/constants/deliveryZones'
-import { CategoryImages } from '@constants/images'
+import DeliveryMap from '@components/ui/DeliveryMap/DeliveryMap'
 import { Box, Divider, Stack, Typography } from '@mui/material'
-import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { useTranslation } from 'react-i18next'
-import { MapContainer, Marker, Polygon, Popup, TileLayer } from 'react-leaflet'
 import './DeliveryTerms.scss'
 
 function DeliveryTerms() {
   const { t } = useTranslation()
-
-  const { customPin } = CategoryImages
-  const customIcon = L.icon({
-    iconUrl: customPin,
-    iconSize: [26, 32],
-    iconAnchor: [13, 32],
-    popupAnchor: [0, -32],
-  })
-
-  const fillBlueOptions = { color: '#49A6D8', fillColor: '#49A6D8', fillOpacity: 0.3 }
-  const fillGreenOptions = { color: '#2FA86D', fillColor: '#2FA86D', fillOpacity: 0.3 }
-  const fillOrangeOptions = { color: '#F29B43', fillColor: '#F29B43', fillOpacity: 0.3 }
-  const fillRedOptions = { color: '#FC6D6B', fillColor: '#FC6D6B', fillOpacity: 0.3 }
 
   return (
     <>
@@ -43,28 +27,7 @@ function DeliveryTerms() {
             height={570}
             className="map-container"
           >
-            <MapContainer
-              center={{ lat: 47.839, lng: 35.137 }}
-              zoom={11}
-              style={{ width: '100%', height: '100%', borderRadius: '8px' }}
-            >
-              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              <Marker position={[47.827101, 35.165119]} icon={customIcon}>
-                <Popup>{t('pages.delivery_terms_page.pickup_point')}</Popup>
-              </Marker>
-              <Polygon pathOptions={fillRedOptions} positions={redZones}>
-                <Popup>{t('pages.delivery_terms_page.delivery_red_zone')}</Popup>
-              </Polygon>
-              <Polygon pathOptions={fillOrangeOptions} positions={orangeZones}>
-                <Popup>{t('pages.delivery_terms_page.delivery_orange_zone')}</Popup>
-              </Polygon>
-              <Polygon pathOptions={fillBlueOptions} positions={blueZones}>
-                <Popup>{t('pages.delivery_terms_page.delivery_blue_zone')}</Popup>
-              </Polygon>
-              <Polygon pathOptions={fillGreenOptions} positions={greenZones}>
-                <Popup>{t('pages.delivery_terms_page.delivery_green_zone')}</Popup>
-              </Polygon>
-            </MapContainer>
+            <DeliveryMap />
             <Stack mt={2}>
               <Typography variant="body1" component={'h1'} fontWeight={500}>
                 {t('pages.delivery_terms_page.min_order_price')}
