bbeef0841a62dd7557af1b8306df9144b254c406
Vakulenko Heorhii <zhora2vakulenko2@gmail.com>
2025-11-19 10:36:39 +0200

    chore: add environment variables

--- client/src/store/services/api.ts
+++ client/src/store/services/api.ts
@@ -2,9 +2,11 @@ import { ApiRoutes } from '@/constants/apiRoutes'
 import { Product } from '@/types'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const BASE_URL = import.meta.env.VITE_API_URL
+
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
     getCatalog: builder.query<Product[], void>({
       query: () => ApiRoutes.CATALOG,
